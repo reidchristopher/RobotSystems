a0b3170e6d08b370cca5f14bd2416ca513981a51
Reid Christopher <reid.christopher@yahoo.com>
2021-04-11 20:59:32 -0700

    Basic forward move script for calibration

---
+++ forward_move.py
@@ -0,0 +1,12 @@
+from picarx_improved import *
+import time
+
+def main():
+    
+    set_dir_servo_angle(0.0)
+    forward(100)
+    time.sleep(1)
+    stop()
+
+if __name__ == "__main__":
+    main()

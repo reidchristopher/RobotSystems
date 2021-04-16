c52bf9889db9d0829c214126d0ef1a6a8aa7be10
Reid Christopher <reid.christopher@yahoo.com>
2021-04-16 02:33:34 -0700

    Using motor controller class in maneuver script

--- maneuvers.py
+++ maneuvers.py
@@ -1,81 +1,83 @@
-from picarx_improved import *
+from motor_controller import MotorController
+import time
 
-def forward_and_backward(angle):
+def forward_and_backward(controller, angle):
     print("Moving forward and back at %.2d degree angle" % angle)
     speed = 50
     t = 1.5
     
-    set_dir_servo_angle(angle)
+    controller.set_dir_servo_angle(angle)
     time.sleep(0.5)
     
-    forward(speed)
+    controller.forward(speed)
     time.sleep(t)
     
-    backward(speed)
+    controller.backward(speed)
     time.sleep(t)
-    stop()
+    controller.stop()
     
-def parallel_park(direction):
+def parallel_park(controller, direction):
     print("Doing parallel park in %s direction" % direction)
     
     speed = 50
     angle_sign = -1 if direction == "l" else 1
     
-    set_dir_servo_angle(0.0)
+    controller.set_dir_servo_angle(0.0)
     time.sleep(0.5)
     
-    forward(speed)
+    controller.forward(speed)
     time.sleep(1.0)
-    stop()
+    controller.stop()
     
-    set_dir_servo_angle(angle_sign * 20.0)
-    backward(speed)
+    controller.set_dir_servo_angle(angle_sign * 20.0)
+    controller.backward(speed)
     time.sleep(1.0)
     
-    set_dir_servo_angle(-angle_sign * 20.0)
-    backward(speed)
+    controller.set_dir_servo_angle(-angle_sign * 20.0)
+    controller.backward(speed)
     time.sleep(1.0)
-    stop()
+    controller.stop()
     
-    set_dir_servo_angle(0.0)
+    controller.set_dir_servo_angle(0.0)
     time.sleep(0.5)
     
-    forward(speed)
+    controller.forward(speed)
     time.sleep(0.75)
-    stop()
+    controller.stop()
     
-def three_point_turn(direction):
+def three_point_turn(controller, direction):
     print("Doing k turn in %s direction" % direction)
     
     speed = 50
     angle_sign = -1 if direction == "l" else 1
     
-    set_dir_servo_angle(angle_sign * 30.0)
+    controller.set_dir_servo_angle(angle_sign * 30.0)
     time.sleep(0.1)
     
-    forward(speed)
+    controller.forward(speed)
     time.sleep(1.0)
-    stop()
+    controller.stop()
     
-    set_dir_servo_angle(-angle_sign * 30.0)
+    controller.set_dir_servo_angle(-angle_sign * 30.0)
     time.sleep(0.1)
     
-    backward(speed)
+    controller.backward(speed)
     time.sleep(1.0)
-    stop()
+    controller.stop()
     
-    set_dir_servo_angle(angle_sign * 30.0)
+    controller.set_dir_servo_angle(angle_sign * 30.0)
     time.sleep(0.1)
     
-    forward(speed)
+    controller.forward(speed)
     time.sleep(1.0)
-    stop()
+    controller.stop()
     
 def main():
     
+    controller = MotorController()
+    
     def input_is_valid(i):
         return i == "f" or i == "p" or i =="k" or i == "q"
-    
     
     while True:
         command = ""
@@ -106,16 +108,16 @@ def main():
                 except ValueError:
                     print(err_str)
 
-            forward_and_backward(angle)
+            forward_and_backward(controller, angle)
         else:
             direction = ""
             while direction != "l" and direction != "r":
                 direction = input("Maneuver (l)eft or (r)ight? ")
                 
             if command == "p":
-                parallel_park(direction)
+                parallel_park(controller, direction)
             elif command == "k":
-                three_point_turn(direction)
+                three_point_turn(controller, direction)
     
 if __name__ == "__main__":
     main()
